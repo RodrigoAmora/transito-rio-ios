66c34015f7c972dd4f4d1cd1538ec19fa399b406
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-29 18:30:48 -0300

    Commit do Projeto

---
+++ Transito Rio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Transito Rio
+//
+//  Created by Rodrigo Amora on 29/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Transito RioTests/Transito_RioTests.swift
@@ -0,0 +1,36 @@
+//
+//  Transito_RioTests.swift
+//  Transito RioTests
+//
+//  Created by Rodrigo Amora on 29/11/23.
+//
+
+import XCTest
+@testable import Transito_Rio
+
+final class Transito_RioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
