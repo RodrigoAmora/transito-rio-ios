484b6ea0ebb1d9fbc76fd7802b127c1cbe734861
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-30 00:55:49 -0300

    Colocando os atributos latitude e longitude como String

---
+++ Transito Rio/Model/Onibus.swift
@@ -0,0 +1,30 @@
+//
+//  Onibus.swift
+//  Transito Rio
+//
+//  Created by Rodrigo Amora on 29/11/23.
+//
+
+import Foundation
+import CoreData
+
+class Onibus: NSCoding, Decodable {
+    
+    // MARK: - Atributes
+    var ordem: String = ""
+    var latitude: String = ""
+    var longitude: String = ""
+    
+    // MARK: - NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(ordem, forKey: "ordem")
+        coder.encode(latitude, forKey: "latitude")
+        coder.encode(longitude, forKey: "longitude")
+    }
+    
+    required init?(coder: NSCoder) {
+        ordem = coder.decodeObject(forKey: "ordem") as! String
+        latitude = coder.decodeObject(forKey: "latitude") as! String
+        longitude = coder.decodeObject(forKey: "longitude") as! String
+    }
+}
