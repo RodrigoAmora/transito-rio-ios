1ff0cedc85d8e2f6649de22762b6b74a90b6f0b0
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-30 03:58:35 -0300

    Pegando a linha do ônibus

--- Transito Rio/Model/Onibus.swift
+++ Transito Rio/Model/Onibus.swift
@@ -14,17 +14,20 @@ class Onibus: NSCoding, Decodable {
     var ordem: String = ""
     var latitude: String = ""
     var longitude: String = ""
+    var linha: String = ""
     
     // MARK: - NSCoding
     func encode(with coder: NSCoder) {
         coder.encode(ordem, forKey: "ordem")
         coder.encode(latitude, forKey: "latitude")
         coder.encode(longitude, forKey: "longitude")
+        coder.encode(linha, forKey: "linha")
     }
     
     required init?(coder: NSCoder) {
         ordem = coder.decodeObject(forKey: "ordem") as! String
         latitude = coder.decodeObject(forKey: "latitude") as! String
         longitude = coder.decodeObject(forKey: "longitude") as! String
+        linha = coder.decodeObject(forKey: "linha") as! String
     }
 }
