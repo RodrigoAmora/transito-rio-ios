f30f2305934c502833f69121e4270273741e774b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-03-09 12:49:43 -0300

    Implementando a tela de sobre

--- Transito Rio/Controller/SobreViewController.swift
+++ Transito Rio/Controller/SobreViewController.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 
-class SobreViewController: UIViewController {
+class SobreViewController: BaseViewController {
 
+    // MARK: - @IBOutlets
+    @IBOutlet weak var lbEmail: UILabel!
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbVersion: UILabel!
+    
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.initViews()
     }
 
+    // MARK: - Methods
+    private func initViews() {
+        self.lbEmail.text = String(localized: "created_by_email")
+        self.lbEmail.textAlignment = .center
+        
+        self.lbName.text = String(localized: "created_by")
+        self.lbName.textAlignment = .center
+        
+        let versionApp = self.getVersionApp()
+        self.lbVersion.text = versionApp
+        self.lbVersion.textAlignment = .center
+    }
+    
+    private func getVersionApp() -> String? {
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+        return "\(String(localized: "created_by_version")) \(appVersion)"
+    }
 
     /*
     // MARK: - Navigation
