a2fa287e392d0b62848339bdee62894073a6c7b0
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-04-25 02:46:39 -0300

    Removendo trecho comentado

--- Transito Rio/Controller/SobreViewController.swift
+++ Transito Rio/Controller/SobreViewController.swift
@@ -38,14 +38,4 @@ class SobreViewController: BaseViewController {
         return "\(String(localized: "created_by_version")) \(appVersion)"
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
